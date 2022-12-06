52ba3b2881271835e3bf3390bc2cf390892fb5cb
Jon Naske <jonnaske@hotmail.com>
2022-12-05 21:06:55 -0800

    day3 part1 complete

---
+++ day3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace day3;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] input = File.ReadAllLines("./input.txt");
+        int value = 0;
+        foreach (string s in input)
+        {
+            value += GetValueOfItem(GetDuplicateItem(s));
+        }
+        Console.WriteLine(value);
+
+    }
+
+    private static int GetDuplicateItem(string pack)
+    {
+        int packSize = pack.Count();
+        string firstHalf = pack.Substring(0, packSize / 2);
+        string secondHalf = pack.Substring(packSize / 2, packSize / 2);
+
+        foreach (char c in firstHalf)
+        {
+            if (secondHalf.Contains(c)) return c;
+        }
+        return Int32.Parse("!");
+
+    }
+
+    private static int GetValueOfItem(int item)
+    {
+        if (item > 96) return item - 96;
+        else return item - 38;
+    }
+}

6c8c8811c8ab545e99416114fa7f2d59a7db82da
Jon Naske <jonnaske@hotmail.com>
2022-12-05 21:56:03 -0800

    d3p2 in progress

--- day3/Program.cs
+++ day3/Program.cs
@@ -5,13 +5,33 @@ internal class Program
     private static void Main(string[] args)
     {
         string[] input = File.ReadAllLines("./input.txt");
-        int value = 0;
-        foreach (string s in input)
-        {
-            value += GetValueOfItem(GetDuplicateItem(s));
-        }
-        Console.WriteLine(value);
+        Part1(input);
+        Part2(input);
+    }
 
+    private static void Part2(string[] input)
+    {
+
+        int value = 0;
+
+        for (int i = 0; i < 300; i += 3)
+        {
+            string elf1 = input[i];
+            string elf2 = input[i + 1];
+            string elf3 = input[i + 2];
+
+            foreach (char item in elf1)
+            {
+                if (elf2.Contains(item))
+                {
+                    if (elf3.Contains(item))
+                    {
+                        value += GetValueOfItem(item);
+                    }
+                }
+            }
+        }
+        Console.WriteLine($"Part 2 value: {value}");
     }
 
     private static int GetDuplicateItem(string pack)
@@ -32,5 +52,15 @@ internal class Program
     {
         if (item > 96) return item - 96;
         else return item - 38;
+    }
+
+    private static void Part1(string[] input)
+    {
+        int value = 0;
+        foreach (string s in input)
+        {
+            value += GetValueOfItem(GetDuplicateItem(s));
+        }
+        Console.WriteLine($"part 1 value: {value}");
     }
 }
